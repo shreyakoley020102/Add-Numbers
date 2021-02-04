63be119183b9c8fc3f89b873c6de1ce5af064c11
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-04 21:01:24 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -6,6 +6,10 @@ app.get("/", function(req,res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req,res){
+  res.send("Thanks");
+});
+
 app.listen(3000, function(){
   console.log("port 3000");
 });
