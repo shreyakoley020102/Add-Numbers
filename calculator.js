cf93b25a38da24ed3e4e49e965aecb2cece7adf1
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-04 21:19:17 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -2,13 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app=express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req,res){
-  res.send("Thanks for postng that");
+
+  var num1 = req.body.num1;
+  var num2 = req.body.num2;
+
+  var result = num1+num2;
+
+  res.send("The result is" + result);
 });
 
 app.listen(3000, function(){
